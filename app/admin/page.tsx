c05505144fc6a8dfa0c6e1ce0e4b1797bba4adf2
Rafael Mallea <108899856+RAFAEL-MALLEA@users.noreply.github.com>
2026-07-13 02:28:28 -0400

    add css to pannel administration

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,13 +1,23 @@
 export default function AdminPage() {
   return (
-    <main className="p-8">
-      <h1 className="text-3xl font-bold text-slate-900">
-        Panel de administración
-      </h1>
-
-      <p className="mt-2 text-slate-600">
-        Aquí gestionarás tus servicios y certificaciones.
-      </p>
+    <>
+    <main className="mb-5 flex flex-wrap items-start justify-start gap-4">
+      <div className="flex flex-col bg-red-300 h-full p-4 rounded-lg">
+        <h1 className="text-2xl font-bold text-slate-500">
+          Panel de administración
+        </h1>
+        <div className="flex flex-col items-center justify-content gap-2 mt-4 w-full">
+          <button>certificaciones</button>
+          <button>servicios</button>
+        </div>
+      </div>
+      <div>
+        <p className="mt-2 text-slate-600">
+          Aquí gestionarás tus servicios y certificaciones.
+        </p>
+      </div>
     </main>
+
+    </>
   );
 }
