d096e6a8891e0f411bf40877c21ca45852f7ce01
Rafael Mallea <108899856+RAFAEL-MALLEA@users.noreply.github.com>
2026-07-13 02:44:04 -0400

    fix

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,18 +1,18 @@
 export default function AdminPage() {
   return (
     <>
-    <main className="mb-5 flex flex-wrap items-start justify-start gap-4">
-      <div className="flex flex-col bg-red-300 h-full p-4 rounded-lg">
+    <main className="flex h-screen gap-4 ">
+      <div className="flex flex-col bg-blue-300 gradient-color h-full p-4 rounded-lg">
         <h1 className="text-2xl font-bold text-slate-500">
           Panel de administración
         </h1>
-        <div className="flex flex-col items-center justify-content gap-2 mt-4 w-full">
+        <div className="flex flex-col items-center justify-content gap-2 mt-10 h-full">
           <button>certificaciones</button>
           <button>servicios</button>
         </div>
       </div>
       <div>
-        <p className="mt-2 text-slate-600">
+        <p className="p-4">
           Aquí gestionarás tus servicios y certificaciones.
         </p>
       </div>
