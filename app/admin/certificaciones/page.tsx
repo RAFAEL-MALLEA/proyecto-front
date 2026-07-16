b2eaee8dc70942de88ce972f3366ca6fc894da06
Rafael Mallea <108899856+RAFAEL-MALLEA@users.noreply.github.com>
2026-07-15 23:08:47 -0400

    fix organization

--- app/admin/certificaciones/page.tsx
+++ app/admin/certificaciones/page.tsx
@@ -1,13 +1,63 @@
-export default function CertificacionesAdminPage() {
-  return (
-    <section>
-      <h2 className="text-2xl font-bold text-slate-900">
-        Gestión de certificaciones
-      </h2>
+"use client";
 
-      <p className="mt-2 text-slate-600">
-        Aquí podrás crear, editar y eliminar certificaciones.
-      </p>
-    </section>
+import { obtenerCertificaciones } from "@/services/certificaciones";
+import { useEffect, useState } from "react";
+import type { Certificacion } from "@/types/Certificacion";
+
+
+
+export default function CertificacionesAdminPage() {
+  const [certificaciones, setCertificaciones] =
+    useState<Certificacion[]>([]);
+
+  const [error, setError] = useState<string | null>(null);
+
+useEffect(() => {
+
+  obtenerCertificaciones()
+    .then((data) => {
+      console.log("Certificaciones recibidas:", data);
+      setCertificaciones(data);
+    })
+    .catch((error) => {
+      console.error("Error certificaciones:", error);
+    });
+}, []);
+
+
+
+  return (
+    <>
+    {error && <p>{error}</p>}
+      <section>
+        <h1>Certificaciones</h1>
+
+        {certificaciones.length === 0 ? (
+          <p>No existen certificaciones registradas.</p>
+        ) : (
+          certificaciones.map((certificacion) => (
+            <article key={certificacion.id}>
+              <h3>{certificacion.nombre_certificacion}</h3>
+
+              <p>
+                Institución:{" "}
+                {certificacion.institucion ?? "No especificada"}
+              </p>
+
+              <p>{certificacion.descripcion}</p>
+
+              <p>Fecha: {certificacion.fecha_obtencion}</p>
+
+              <p>
+                Estado:{" "}
+                {certificacion.estado === "Completado"
+                  ? "Completado"
+                  : "Terminado"}
+              </p>
+            </article>
+          ))
+        )}
+      </section>
+      </>
   );
 }
--- app/admin/servicios/page.tsx
+++ app/admin/servicios/page.tsx
@@ -1,13 +1,47 @@
-export default function ServiciosAdminPage() {
-  return (
-    <section>
-      <h2 className="text-2xl font-bold text-slate-900">
-        Gestión de servicios
-      </h2>
+"use client";
 
-      <p className="mt-2 text-slate-600">
-        Aquí podrás crear, editar y eliminar servicios.
-      </p>
-    </section>
+import type { Servicio } from "@/types/Servicio";
+import { obtenerServicios } from "@/services/servicios";
+import { useEffect, useState } from "react";
+
+export default function ServiciosAdminPage() {
+  const [servicios, setServicios] = useState<Servicio[]>([]);
+
+
+  const [error, setError] = useState<string | null>(null);
+
+
+useEffect(() => {
+  obtenerServicios()
+    .then((data) => {
+      console.log("Servicios recibidos:", data);
+      setServicios(data);
+    })
+    .catch((error) => {
+      console.error("Error servicios:", error);
+    });
+
+}, []);
+
+
+
+  return (
+    <>
+    {error && <p>{error}</p>}
+      <section>
+        <h1>Servicios</h1>
+
+        {servicios.length === 0 ? (
+          <p>No existen servicios registrados.</p>
+        ) : (
+          servicios.map((servicio) => (
+            <div key={servicio.id}>
+              <h3>{servicio.Nombre_servicio}</h3>
+              <p>${Number(servicio.valor).toLocaleString("es-CL")}</p>
+            </div>
+          ))
+        )}
+      </section>
+      </>
   );
 }
